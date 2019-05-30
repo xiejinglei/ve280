e00dae903b4f5deb3b58c41dc70db29fb7a70255
wangsoap <48651361+wangsoap@users.noreply.github.com>
2019-05-30 21:47:58 +0800

    Update duffinian.cpp

--- labs/lab1/duffinian.cpp
+++ labs/lab1/duffinian.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <cassert>
-
+#include <vector>
 using namespace std;
 
 bool is_duffinian(int num) {
-    // TODO: Your implementation here
-    return false;
+    int sum=0;
+    vector<int>divisors;
+    for (int i=1;i<=num/2;i++){
+        if (num%i==0){
+            sum+=i;
+            divisors.push_back(i);
+        }
+    }
+    for (int i=1;i<divisors.size();i++){
+        if(sum%divisors[i]==0)return false;
+    }
+        return true;
 }
 
 void test_duffinian() {
     assert(!is_duffinian(6));  // 6 is not a Duffinian number
+    assert(is_duffinian(35));
+    assert(is_duffinian(2431));
+    assert(!is_duffinian(114));
     // TODO: Add more test cases
     cout << "Duffinian number tests passed!" << endl;
 }
