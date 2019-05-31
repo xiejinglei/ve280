054b025d6129875b07cd483209ce6ad7431b91f2
Kevin Chen <31037237+chenty0704@users.noreply.github.com>
2019-05-31 13:14:45 +0800

    Implemented alternating.cpp & duffinian.cpp

--- labs/lab1/alternating.cpp
+++ labs/lab1/alternating.cpp
@@ -4,12 +4,28 @@
 using namespace std;
 
 bool is_alternating(int num, int base) {
-    // TODO: Your implementation here
-    return false;
+    int digit, last_digit;
+    last_digit = num % base;
+    num /= base;
+    while (num > 0) {
+        digit = num % base;
+        if ((digit + last_digit) % 2 == 0)
+            return false;
+        last_digit = digit;
+        num /= base;
+    }
+    return true;
 }
 
 void test_alternating() {
     assert(!is_alternating(24, 10));  // 24 is not an alternating number in base 10
-    // TODO: Add more test cases
+    assert(is_alternating(1, 10));
+    assert(is_alternating(2, 10));
+    assert(is_alternating(989, 10));
+    assert(is_alternating(1234543210, 10));
+    assert(is_alternating(1, 2));
+    assert(is_alternating(2, 2));
+    assert(is_alternating(5, 2));
+    assert(is_alternating(21, 2));
     cout << "Alternating number tests passed!" << endl;
 }
--- labs/lab1/duffinian.cpp
+++ labs/lab1/duffinian.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 using namespace std;
 
 bool is_duffinian(int num) {
-    // TODO: Your implementation here
-    return false;
+    int sum_divisors = 0;
+    for (int i = 1; i <= num; ++i) {
+        if (num % i == 0)
+            sum_divisors += i;
+    }
+
+    for (int i = 2; i < min(sqrt(num), sqrt(sum_divisors)); ++i) {
+        if (num % i == 0 && sum_divisors % i == 0)
+            return false;
+    }
+    return true;
 }
 
 void test_duffinian() {
     assert(!is_duffinian(6));  // 6 is not a Duffinian number
-    // TODO: Add more test cases
+    assert(is_duffinian(35));
     cout << "Duffinian number tests passed!" << endl;
 }
