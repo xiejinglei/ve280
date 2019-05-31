a350cb2b8ba049ec473f8913dea33987c74dd970
zhaomuhan <zhaomuhan@sjtu.edu.cn>
2019-05-30 22:43:22 -0700

    Finish p3

--- labs/lab1/alternating.cpp
+++ labs/lab1/alternating.cpp
@@ -3,13 +3,38 @@
 
 using namespace std;
 
+int SameSign(int num1, int num2){
+	if(num1 % 2 == num2 % 2){return 1;}
+	else {return 0;}
+}
 bool is_alternating(int num, int base) {
     // TODO: Your implementation here
-    return false;
+    int flag = 0;
+    int b = base;
+    while(num>0){
+    int temp1, temp2;	
+    num /= b;    
+    temp1 = num % b;
+    num = num/b;
+    temp2 = num % b;
+    if (!SameSign(temp1,temp2)){flag=1;}
+    else{continue;}
+
+    }
+    if(flag==1){cout << "is alternative" << endl;}
+    else {cout << "not alternative"<<endl;}
+	return false;
 }
 
 void test_alternating() {
     assert(!is_alternating(24, 10));  // 24 is not an alternating number in base 10
     // TODO: Add more test cases
+      assert(!is_alternating(123454320, 10));
+       assert(!is_alternating(10101,2));
     cout << "Alternating number tests passed!" << endl;
+}
+
+int main(){
+test_alternating();
+return 0;
 }
