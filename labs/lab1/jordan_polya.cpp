904074dfcb1dac4bc153d67703993bc27212d34c
wangsoap <48651361+wangsoap@users.noreply.github.com>
2019-05-31 10:51:12 +0800

    Update jordan_polya.cpp

--- labs/lab1/jordan_polya.cpp
+++ labs/lab1/jordan_polya.cpp
@@ -3,13 +3,26 @@
 
 using namespace std;
 
+int factorial(int num){
+    if (num==1)return 1;
+    return (factorial(num-1)*num);
+}
 bool is_jordan_polya(int num) {
-    // TODO: Your implementation here
+    int n=2;
+    if (num==1)return true;
+    while (factorial(n)<=num){
+        if(num%factorial(n)==0 && is_jordan_polya(num/factorial(n)))return true;
+        n++;
+    }
     return false;
 }
-
 void test_jordan_polya() {
     assert(!is_jordan_polya(5));  // 5 is not a Jordan-Polya number
-    // TODO: Add more test cases
+    assert(is_jordan_polya(92160));
+    assert(is_jordan_polya(24));
+    assert(is_jordan_polya(17280));
+    assert(!is_jordan_polya(1998));
+    assert(is_jordan_polya(72));
+    assert(is_jordan_polya(1088640));
     cout << "Jordan-Polya number tests passed!" << endl;
 }
