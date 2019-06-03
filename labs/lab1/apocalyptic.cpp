258b3ac0e3d73540f18a957c345d232195da03ba
TimothyGe <timothyge19@gmail.com>
2019-06-03 12:34:00 +0800

    Update apocalyptic.cpp

--- labs/lab1/apocalyptic.cpp
+++ labs/lab1/apocalyptic.cpp
@@ -30,6 +30,39 @@ namespace item084 {
     }
 }
 
+namespace TimothyGe {
+	#define BASE 10
+	typedef vector<int> Bign;
+
+	Bign& self_times(Bign &a, int b) {
+		int c = 0;
+		for (int i = 0; i < a.size() || c != 0; ++i) {
+			if (i >= a.size()) a.push_back(0); // carry 1
+			a[i] = a[i] * b + c;
+			c = a[i] / BASE;
+			a[i] %= BASE;
+		}
+		return a;
+	}
+
+	bool is_apocalyptic(int num) {
+		Bign a;
+		a.push_back(1); // init as 1
+
+		for (int i = 0; i < num; ++i) {
+			self_times(a, 2);
+		}
+
+		if (a.size() <= 2) return false;
+		for (int i = 0; i < a.size() - 2; ++i) {
+			if (a[i] == 6 && a[i+1] == 6 && a[i+2] == 6) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
 void test_apocalyptic() {
     assert(!item084::is_apocalyptic(1));
     assert(item084::is_apocalyptic(157));
@@ -38,5 +71,13 @@ void test_apocalyptic() {
     assert(item084::is_apocalyptic(931));
     assert(!item084::is_apocalyptic(1700));
     assert(item084::is_apocalyptic(1701));
+	
+	assert(!TimothyGe::is_apocalyptic(1));
+    assert(TimothyGe::is_apocalyptic(157));
+    assert(!TimothyGe::is_apocalyptic(200));
+    assert(TimothyGe::is_apocalyptic(220));
+    assert(TimothyGe::is_apocalyptic(931));
+    assert(!TimothyGe::is_apocalyptic(1700));
+    assert(TimothyGe::is_apocalyptic(1701));
     cout << "Apocalyptic number tests passed!" << endl;
 }
