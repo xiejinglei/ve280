e38d4a3b3a4d4515f932439896934572c7b8e956
Zian Ke <kezian@umich.edu>
2019-06-12 12:40:50 +0800

    Add comments to test.cpp

--- labs/lab2/test.cpp
+++ labs/lab2/test.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    cord_t c;
-
-    c = cord_new("");
+    // Test cord representing the empty string
+    cord_t c = cord_new("");
     assert(c == nullptr);
     assert(cord_length(c) == 0);
 
+    // Test basic leaf node
     cord_t c1 = cord_new("1234");
     assert(cord_length(c1) == 4);
 
+    // Test basic concatenation node
     cord_t c3 = cord_join(c1, cord_new("56"));
     assert(cord_length(c3) == 6);
     assert(cord_tostring(c3) == "123456");
 
+    // Test cord_charat
     cord_t c4 = cord_new("012345");
     assert(cord_charat(c4, 0) == '0');
     assert(cord_charat(c4, 1) == '1');
@@ -25,15 +27,18 @@ int main(int argc, char **argv) {
     assert(cord_charat(c5, 0) == '2');
     assert(cord_charat(c5, 1) == '3');
 
+    // Test cord_sub, where substr is the complete string
     cord_t c6 = cord_new("012345");
     cord_t c7 = cord_sub(c6, 0, cord_length(c6));
     assert(c7 == c6);
 
+    // Test cord_sub, where substr is represented by one child node
     cord_t c8 = cord_new("6789");
     cord_t c9 = cord_join(c6, c8);
     cord_t c10 = cord_sub(c9, 6, 10);
     assert(c10 == c8);
 
+    // Test the three examples of cord_sub in README.md
     cord_t R = cord_join(cord_join(cord_new("t"), cord_new("otally")), cord_join(cord_new("efficien"), cord_new("t")));
     assert(cord_tostring(R) == "totallyefficient");
 
@@ -49,6 +54,7 @@ int main(int argc, char **argv) {
     cord_t R3 = cord_sub(R, 2, 11);
     assert(cord_tostring(R3) == "tallyeffi");
 
+    // Test cord validity
     assert(is_cord(c4));
     assert(is_cord(c5));
     assert(is_cord(c6));
